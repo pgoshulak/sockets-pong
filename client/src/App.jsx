d97f3052fcef45ebf72cba12e78f687174a75d26
Peter Goshulak <pgoshulak@gmail.com>
2018-05-16 23:22:17 -0400

    Fix ball physics to detect paddle collision and speed up on bounce

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,10 @@ import './App.css';
 import Game from './Game';
 
 const SOCKET_ADDRESS = 'ws://localhost:3001'
-// const 
+const X_MIN = 2, X_MAX = 98;
+const Y_MIN = 0, Y_MAX = 100;
+const BALL_SPEED_INCREMENT = 1.05;
+const singlePlayer = true
 
 const PlayerCounter = ({count}) => {
   return <div>Count is {count}</div>
@@ -14,13 +17,15 @@ class App extends Component {
     super();
     this.state = {
       playerPos: [50,50],
+      playerSize: [10, 10],
       currentPlayer: -1,
       playerScore: [0,0],
       ball: {
-        x: 0,
-        y: 0,
+        x: 50,
+        y: 50,
         dx: 2,
-        dy: 0.5
+        dy: 0.5,
+        speed: 1.0
       },
       upArrowPressed: false,
       downArrowPressed: false
@@ -103,26 +108,85 @@ class App extends Component {
 
   updateKeys = () => {
     if (this.state.upArrowPressed) {
-      this.incrementPlayerPos(this.state.currentPlayer, 1, this.sendCurrentPlayerPos)
+      this.incrementPlayerPos(this.state.currentPlayer, -1, this.sendCurrentPlayerPos)
     }
     if (this.state.downArrowPressed) {
-      this.incrementPlayerPos(this.state.currentPlayer, -1, this.sendCurrentPlayerPos)
+      this.incrementPlayerPos(this.state.currentPlayer, +1, this.sendCurrentPlayerPos)
     }
   }
 
   updateBall = () => {
     const ball = {...this.state.ball}
-    if (ball.x > 100 || ball.x < 0) {
-      ball.dx *= -1
+    const playerPos = this.state.playerPos;
+    const playerSize = this.state.playerSize
+    // Check along x=0 side
+    if (ball.x < X_MIN) {
+      // Check if ball is within the player's paddle
+      if (Math.abs(ball.y - playerPos[0]) <= playerSize[0]/2) {
+        // Reflect the ball (TODO: calculate new angle)
+        ball.dx *= -1
+        // Increase the speed
+        ball.speed *= BALL_SPEED_INCREMENT
+      } else {
+        // Reset score here
+        this.setWinner(1)
+        return
+      }
     }
-    if (ball.y > 100 || ball.y < 0) {
+    if (ball.x > X_MAX) {
+      // Check if ball is within the player's paddle
+      if (singlePlayer || Math.abs(ball.y - playerPos[1]) <= playerSize[1]/2) {
+        // Reflect the ball (TODO: calculate new angle)
+        ball.dx *= -1
+        // Increase the speed
+        ball.speed *= BALL_SPEED_INCREMENT
+      } else {
+        // Reset score here
+        this.setWinner(0)
+        return
+      }
+    }
+    // Reflect ball along top/bottom borders
+    if (ball.y > Y_MAX || ball.y < Y_MIN) {
       ball.dy *= -1
     }
 
-    ball.x += ball.dx
-    ball.y += ball.dy
+        // Increment ball location
+    ball.x += ball.dx * ball.speed
+    ball.y += ball.dy * ball.speed
     this.setState({ ball })
   }
+
+  setWinner(playerNum) {
+    // Get and increment the winner's score
+    const playerScore = [...this.state.playerScore]
+    playerScore[playerNum]++
+    this.setState({playerScore})
+    // Reset the ball position
+    this.resetBall();
+  }
+
+  resetBall = () => {
+    let ball = {
+      x: 50,
+      y: 50,
+      dx: 0,
+      dy: 0,
+      speed: 1.0
+    }
+    this.setState({ ball });
+    ball = {
+      x: 50,
+      y: 50,
+      dx: 2.0,
+      dy: 0.5,
+      speed: 1.0
+    }
+    setTimeout(() => {
+      this.setState({ ball })
+    }, 1000)
+  }
+
 
   sendNewServerMessage = (data) => {
     this.socket.send(JSON.stringify(data));
@@ -148,10 +212,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello player { this.state.currentPlayer }
+        Hello player { this.state.currentPlayer } <br/>
+        Player 0: {this.state.playerScore[0]} /// Player 1: {this.state.playerScore[1]}
         <PlayerCounter count={ this.state.playerPos[0] } />
         <PlayerCounter count={ this.state.playerPos[1] } />
-        <Game p0Position={this.state.playerPos[0]} p1Position={this.state.playerPos[1]} ball={this.state.ball}/>
+        <Game playerPos={this.state.playerPos} playerSize={this.state.playerSize} ball={this.state.ball}/>
       </div>
     );
   }
--- client/src/Game/index.jsx
+++ client/src/Game/index.jsx
@@ -13,14 +13,17 @@ class Game extends Component {
     const scaleX = surfaceWidth / 100;
     const scaleY = surfaceHeight / 100;
     return ( 
-      <Stage width={surfaceWidth} height={surfaceHeight}>
-        <Layer width={100} height={100} scale={{x: scaleX, y: scaleY}}>
-          <Rect x={0} y={0} width={100} height={100} fill={'grey'}/>
-          <Rect x={1} y={95 - this.props.p0Position} width={1} height={10} fill={'black'}/>
-          <Rect x={98} y={95 - this.props.p1Position} width={1} height={10} fill={'black'}/>
-          <Rect x={this.props.ball.x} y={this.props.ball.y} width={10/scaleX} height={10/scaleY} fill={'black'} />
-        </Layer>
-      </Stage>
+      <div>
+        ball speed = {this.props.ball.speed}
+        <Stage width={surfaceWidth} height={surfaceHeight}>
+          <Layer width={100} height={100} scale={{x: scaleX, y: scaleY}}>
+            <Rect x={0} y={0} width={100} height={100} fill={'grey'}/>
+            <Rect x={0} y={-5 + this.props.playerPos[0]} width={1} height={10} fill={'black'}/>
+            <Rect x={99} y={-5 + this.props.playerPos[1]} width={1} height={10} fill={'black'}/>
+            <Rect x={this.props.ball.x} y={this.props.ball.y} width={10/scaleX} height={10/scaleY} fill={'black'} />
+          </Layer>
+        </Stage>
+      </div>
      )
   }
 }
